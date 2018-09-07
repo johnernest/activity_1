3959221a2f748e0c44d1cad8c629a8518a0c1398
johnernest <jnestrog@gmail.com>
2018-09-07 20:42:47 +0800

    edit 2

--- prototype/template/header.php
+++ prototype/template/header.php
@@ -1,6 +1,6 @@
 <header>
             <nav id="header-top" class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">PHP Tutorial - Basic</a>
+                <a class="navbar-brand" href="#">PHP Tutorial - Basic 2</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
                     aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
